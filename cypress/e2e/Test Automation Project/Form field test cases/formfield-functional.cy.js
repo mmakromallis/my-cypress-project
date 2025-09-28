db6f43cad669a40092d164e006be88fca9c4dbbd
Manos Makromallis <mmakromallis@epignosishq.com>
2025-09-28 20:40:04 +0300

    sample commit

--- cypress/e2e/Test Automation Project/Form field test cases/formfield-functional.cy.js
+++ cypress/e2e/Test Automation Project/Form field test cases/formfield-functional.cy.js
@@ -4,5 +4,13 @@ describe('Form field scenarios', () => {
 
     it('Verify that the user can input text into Name, Email, Phone & Address fields', () =>{
         cy.visit('/')
+
+        cy.url().should('include', 'testautomationpractice')
+
+        cy.get('.titlewrapper')
+            .should('be.visible')
+            .should('contain.text', 'Automation Testing Practice')
+
+        cy.get('')
     })
 })
